7bea1c973fd6d5f3df4c817ee49ab12d60585814
PAULO MATIAS <pgabrieldeveloper@outlook.com>
2025-03-29 11:02:12 -0300

    feat(domain): criando domain services de Simulação e Metricas

---
+++ internal/domain/quote/quote_services.go
@@ -0,0 +1,25 @@
+package quote
+
+type QuoteService struct {
+	SmltPort    SimulateQuoteOutPutPort
+	MetricsPort MetricsOutputPort
+}
+
+func NewQuoteService(portSmlt SimulateQuoteOutPutPort, portMetrics MetricsOutputPort) *QuoteService {
+	return &QuoteService{
+		SmltPort:    portSmlt,
+		MetricsPort: portMetrics,
+	}
+}
+
+func (qs *QuoteService) SimulateQuote(quote QuoteRequest) ([]Offer, error) {
+
+	if err := quote.Validate(); err != nil {
+		return nil, err
+	}
+	return qs.SmltPort.Execute(quote)
+}
+
+func (qs *QuoteService) GetQuoteMetrics(lastQuotes int) ([]Metrics, error) {
+	return qs.MetricsPort.Execute(lastQuotes)
+}
